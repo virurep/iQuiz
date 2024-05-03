a5e0833ff9024652d9768194fc7e02d4c860695c
virurepalle <virurep@uw.edu>
2024-05-02 17:20:46 -0700

    Initial Commit

---
+++ iQuiz/iQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizApp.swift
+//  iQuiz
+//
+//  Created by Viru Repalle on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct iQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
